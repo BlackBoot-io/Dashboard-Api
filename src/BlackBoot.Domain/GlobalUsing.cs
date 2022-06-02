f6e0cc163707753b6a6e5e821b0e85aa360a03cd
Mohammad <Mohammad@DESKTOP-74VHSM6>
2022-06-02 10:17:51 +0430

    Change GlobalUsing

--- src/BlackBoot.Domain/GlobalUsing.cs
+++ src/BlackBoot.Domain/GlobalUsing.cs
@@ -3,4 +3,3 @@ global using BlackBoot.Domain.Consts;
 global using BlackBoot.Shared.Core;
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
-
