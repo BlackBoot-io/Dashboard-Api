7342de785658bec7fe2cca2961668792322ec271
Mohammad Mokhtari <Mohammad.Mokhtari86@gmail.com>
2022-06-10 22:10:49 +0430

    change jwt expire time and some refactor

--- src/BlackBoot.Api/Controllers/NotificationController.cs
+++ src/BlackBoot.Api/Controllers/NotificationController.cs
@@ -8,14 +8,14 @@ public class NotificationController : BaseController
         => _notificationService = notificationService;
 
     [HttpGet]
-    public async Task<ActionResult<List<Notification>>> GetAllAsync(Guid userId, CancellationToken cancellationToken)
+    public async Task<IActionResult> GetAllAsync(Guid userId, CancellationToken cancellationToken)
         => Ok(await _notificationService.GetAllAsync(userId, cancellationToken));
 
     [HttpPost]
-    public async Task<ActionResult<List<Notification>>> AddAsync(Notification notification, CancellationToken cancellationToken)
+    public async Task<IActionResult> AddAsync(Notification notification, CancellationToken cancellationToken)
         => Ok(await _notificationService.AddAsync(notification, cancellationToken));
 
     [HttpDelete]
-    public async Task<ActionResult<List<Notification>>> DeleteAsync(Guid userId, int id, CancellationToken cancellationToken)
+    public async Task<IActionResult> DeleteAsync(Guid userId, int id, CancellationToken cancellationToken)
         => Ok(await _notificationService.DeleteAsync(userId, id, cancellationToken));
 }
