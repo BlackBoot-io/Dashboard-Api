c7c87aff04e041ea47e6e1037f03e10c9af8aadd
mohammadKarimi <mha.karimi@gmail.com>
2022-07-17 12:29:23 +0430

    Add Qr Generator Change Transaction Ordering Add Date on Adding new transaction Add FileLogger to share project Change crowdSaleSchedule decimal fields

---
+++ src/BlackBoot.Shared/Core/Log/ExceptionDetails.cs
@@ -0,0 +1,9 @@
+﻿#nullable disable
+namespace BlackBoot.Shared.Core;
+public class ExceptionDetails
+{
+    public string MethodName { get; set; }
+    public string Parameters { get; set; }
+    public int ExceptionLineNumber { get; set; }
+}
+
